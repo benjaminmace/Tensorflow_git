ee7274ac2498140dba28b64e0b43b77aabad9a5a
Benjamin Mace <mace.benjamin.e@gmail.com>
2020-09-29 11:12:53 -0500

    Minor unknown changes to Lesson_3_MNIST.py, 1112 29 Sep 20

--- TF_Basics_Aladdin_YouTube/Lesson_3_MNIST.py
+++ TF_Basics_Aladdin_YouTube/Lesson_3_MNIST.py
@@ -6,6 +6,9 @@ from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.datasets import mnist
 
+if tf.test.gpu_device_name():
+    print('Default GPU Device: {}'.format(tf.test.gpu_device_name()))
+
 (x_train, y_train) , (x_test, y_test) = mnist.load_data()
 x_train = x_train.reshape(-1, 28*28).astype('float32') / 255.0
 x_test = x_test.reshape(-1, 28*28).astype('float32') / 255.0
